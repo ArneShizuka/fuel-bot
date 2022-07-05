98b60c16e95ebe425b1cd159ae9b8e63aeee337c
AngeloIacona <iaconaangelo.ai@gmail.com>
2022-07-05 21:07:30 +0200

    feat(main): add basic bot config

--- src/main.py
+++ src/main.py
@@ -1,5 +1,19 @@
+import os
+import dotenv
+
+from telegram.ext import Updater
+
+
 def main() -> None:
-    ...
+    dotenv.load_dotenv()
+    updater = Updater(os.getenv('TOKEN'),
+                      request_kwargs={
+                          'read_timeout': 20,
+                          'connect_timeout': 20},
+                      use_context=True)
+
+    updater.start_polling()
+    updater.idle()
 
 
 if __name__ == '__main__':
