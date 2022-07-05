f938494dbb04115a33aef3707338076a98899110
AngeloIacona <iaconaangelo.ai@gmail.com>
2022-07-06 00:39:59 +0200

    chore: update main.py

--- src/main.py
+++ src/main.py
@@ -1,20 +1,20 @@
 import os
-import dotenv
 
+import dotenv
 from telegram.ext import Updater
 
 
 def main() -> None:
     dotenv.load_dotenv()
-    updater = Updater(os.getenv('TOKEN'),
-                      request_kwargs={
-                          'read_timeout': 20,
-                          'connect_timeout': 20},
-                      use_context=True)
+    updater = Updater(
+        os.getenv("TOKEN"),
+        request_kwargs={"read_timeout": 20, "connect_timeout": 20},
+        use_context=True,
+    )
 
     updater.start_polling()
     updater.idle()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
