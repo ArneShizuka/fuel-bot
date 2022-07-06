d11fd0a2b1715aa0e15690149a94c3e81c4f9e2a
AngeloIacona <iaconaangelo.ai@gmail.com>
2022-07-06 11:07:50 +0200

    udpate main.py

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ import os
 import dotenv
 from telegram.ext import Updater
 
+from modules.handlers import add_handlers
+
 
 def main() -> None:
     dotenv.load_dotenv()
@@ -11,6 +13,8 @@ def main() -> None:
         request_kwargs={"read_timeout": 20, "connect_timeout": 20},
         use_context=True,
     )
+
+    add_handlers(updater.dispatcher)
 
     updater.start_polling()
     updater.idle()
